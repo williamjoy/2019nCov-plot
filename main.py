1e396ea80469dff6b974d879783ea5910a5a241c
William Wei <william.wei@demonware.net>
2020-02-13 12:22:35 -0800

    initial data

---
+++ main.py
@@ -0,0 +1,69 @@
+import csv
+import numpy as np
+
+from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as mfm
+import argparse
+import re
+
+FONT_FILE = '/System/Library/Fonts/STHeiti Light.ttc'
+prop = mfm.FontProperties(fname=FONT_FILE)
+
+TIME_ZERO = datetime.fromisoformat('2020-01-24')
+FILE_NAME = "./hubei.csv"
+FILE_NAME = "./DXYArea.csv"
+_, ax = plt.subplots()
+
+ax.set(xlim=(0, 4000000))
+
+parser = argparse.ArgumentParser(description='Plot 2019nCov Confirmed cases')
+parser.add_argument('--province', help='province name')
+
+parser.add_argument('--exclude', help='exclude key word')
+args = parser.parse_args()
+black_list_re =  re.compile(args.exclude) if args.exclude else None
+print(args)
+
+plt.xlabel(f'day from {TIME_ZERO}', fontproperties=prop)
+
+ax.set(xlim=(0, (datetime.now().replace(hour=0, minute=0) -
+                 TIME_ZERO).total_seconds()/3600/24 + 2))
+with open(FILE_NAME, newline='',  encoding='utf-8-sig') as csvfile:
+    reader = csv.DictReader(csvfile)
+    if args.province:
+        value_key = 'city_confirmedCount'
+        name_key = 'cityName'
+        data = list(filter(lambda x:  x.get(
+            'provinceName', "").startswith(args.province), reader))
+    else:
+        value_key = 'province_confirmedCount'
+        name_key = 'provinceName'
+        data = reader
+
+    data_by_geo = {}
+    for row in data:
+        geo_location = row.get(name_key, "None")
+        if black_list_re and black_list_re.findall(geo_location):
+            continue
+        value = int(row.get(value_key, 0))
+        updateTime = datetime.fromisoformat(row.get('updateTime'))
+        day_diff = (updateTime.replace(hour=0, minute=0, second=0,
+                                       microsecond=0) - TIME_ZERO).total_seconds()/3600/24
+
+        data_by_geo.setdefault(geo_location, {})
+
+        #truncate to day
+        if data_by_geo.get(geo_location).get(day_diff, 0) < value:
+            data_by_geo[geo_location][day_diff] = value
+
+    print(len(data_by_geo))
+
+    for geo_location, points in data_by_geo.items():
+        if not points:
+            continue
+        print(geo_location, max(points.values()))
+        plt.scatter(points.keys(), points.values(), label=geo_location)
+ax.legend(loc='upper left', prop=prop)
+ax.grid(True)
+plt.show()
