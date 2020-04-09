0b8e948439944f5ca732dad3926f98c32e36ccd6
William Wei <william.wei@demonware.net>
2020-04-09 13:12:12 -0700

    Add more graphs: daily new cases/tested cases

--- Makefile
+++ Makefile
@@ -8,7 +8,9 @@ others:
 	#python3 main.py  --exclude 湖北 --save-file 'output/China-Excluding-Hubei'
 	#python3 main.py  --exclude 武汉 --province 湖北 --save-file 'output/Hubei-Excluding-Wuhan'
 	#python3 main.py  --save-file 'output/China'
-	python3 canada.py  --save-file 'output/Canada'
+	python3 canada.py  --save-file 'output/Canada_Confirmed_Cases' --title 'Canada Confirmed Cases'
+	python3 canada.py  --save-file 'output/Canada_Daily_New_Cases' --value-column numtoday --title 'Canada Daily New Cases'
+	python3 canada.py  --save-file 'output/Canada_Tested_Cases' --value-column numtested --title 'Canada Tested Cases'
 provinces:
 	tail -n +2 DXYArea.csv|cut -d, -f 1|sort -u |xargs -n1 -I '{}' python3 main.py --province '{}' --save-file 'output/{}'
 
--- canada.py
+++ canada.py
@@ -28,28 +28,33 @@ FILE_NAME = "./canada.csv"
 _, ax = plt.subplots()
 ax.yaxis.tick_right()
 parser = argparse.ArgumentParser(description='Plot 2019nCov Confirmed cases')
-parser.add_argument('--province', help='province name')
+parser.add_argument('--value-column', help='project on column name, default: numconf', default='numconf')
 parser.add_argument('--save-file', help='file name to save')
 parser.add_argument('--exclude', help='exclude data, regular expression')
+parser.add_argument('--title', help='graph title', default='Confirmed Cases')
 args = parser.parse_args()
+print (args)
 black_list_re = re.compile(args.exclude) if args.exclude else None
 
 plt.xlabel(f'Date', fontproperties=prop)
 
 with open(FILE_NAME, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    value_key = 'numconf'
+    value_key = args.value_column
     name_key = 'prname'
     title = 'Canada'
     data = reader
 
-    plt.ylabel(f'{title} confirmed cases', fontproperties=prop)
+    plt.ylabel(f'{args.title}', fontproperties=prop)
     data_by_geo = {}
     for row in data:
         geo_location = row.get(name_key, "None")
         if black_list_re and black_list_re.findall(geo_location):
             continue
-        value = int(row.get(value_key, 0))
+        try:
+            value = int(row.get(value_key, 0))
+        except ValueError as e:
+            continue
         updateTime = datetime.strptime(row.get('date'), '%d-%m-%Y')
         if updateTime > datetime.strptime('01-03-2020', '%d-%m-%Y'):
             data_by_geo.setdefault(geo_location, {})
@@ -67,7 +72,7 @@ with open(FILE_NAME, newline='') as csvfile:
                       label=f'{geo_location}: {max(points.values())}')
 ax.legend(loc='upper left', prop=prop)
 ax.grid(True)
-plt.title('confirmed cases')
+plt.title(args.title)
 plt.gcf().autofmt_xdate()
 if args.save_file:
     plt.savefig(f'{args.save_file}.svg', format='svg',)
